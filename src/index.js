e2072e9963034a76dcef86fe625f8ff68d2c443a
mag <polina.arutyunyan.08@mail.ru>
2023-06-22 01:03:54 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,33 +3,12 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import { cwd } from 'node:process';
-import _ from 'lodash';
 import parsers from './parsers.js';
+import generateDiff from './gendiff.js';
+import getFormatted from './formatters/index-formatters.js';
 
-const getAbsoluteFilePath = (filepath) => path.resolve(cwd(), filepath);
+const getAbsoluteFilePath = (filepath) => path.resolve(cwd(), '__fixtures__', filepath);
 
-const generateDiff = (obj1, obj2) => {
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-    const keys = _.sortBy(_.union(keys1, keys2));
-    const result = {};
-    keys.map((key) => {
-        if (!Object.hasOwn(obj1, key)) {
-            result[`+ ${key}`] = obj2[key];
-        } else if (!Object.hasOwn(obj2, key)) {
-            result[`- ${key}`] = obj1[key];
-        } else if (obj1[key] !== obj2[key]) {
-            result[`- ${key}`] = obj1[key];
-            result[`+ ${key}`] = obj2[key];
-        } else {
-            result[`  ${key}`] = obj1[key];
-        }
-        return result;
-    });
-    const arr = _.toPairs(result);
-    const newArr = arr.map((item) => item.join(': ')).join('\n  ');
-    return `{\n  ${newArr}\n}`;
-};
 export default (filepath1, filepath2) => {
     const absoluteFilepath1 = getAbsoluteFilePath(filepath1);
     const absoluteFilepath2 = getAbsoluteFilePath(filepath2);
@@ -37,5 +16,5 @@ export default (filepath1, filepath2) => {
     const parse = parsers(format);
     const data1 = parse(readFileSync(absoluteFilepath1, 'utf-8'));
     const data2 = parse(readFileSync(absoluteFilepath2, 'utf-8'));
-    return generateDiff(data1, data2);
+    return getFormatted(generateDiff(data1, data2));
 };
