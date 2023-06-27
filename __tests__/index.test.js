acda9e0806a7879b4a63306aa1ad49cef2c5f696
mag <polina.arutyunyan.08@mail.ru>
2023-06-27 16:25:27 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,20 +10,38 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('json check', () => {
+test('json check stylish', () => {
     const file1Path = getFixturePath('file1.json');
     const file2Path = getFixturePath('file2.json');
-    const resultPath = getFixturePath('result.file.txt');
+    const resultPath = getFixturePath('result.stylish.txt');
     const result = readFileSync(resultPath, 'utf8');
     const diff = gendiff(file1Path, file2Path);
     expect(diff).toBe(result);
 });
 
-test('yml check', () => {
+test('yml check stylish', () => {
     const file1Path = getFixturePath('file1.yml');
     const file2Path = getFixturePath('file2.yml');
-    const resultPath = getFixturePath('result.file.txt');
+    const resultPath = getFixturePath('result.stylish.txt');
     const result = readFileSync(resultPath, 'utf8');
     const diff = gendiff(file1Path, file2Path);
+    expect(diff).toBe(result);
+});
+
+test('json check plain', () => {
+    const file1Path = getFixturePath('file1.json');
+    const file2Path = getFixturePath('file2.json');
+    const resultPath = getFixturePath('result.plain.txt');
+    const result = readFileSync(resultPath, 'utf8');
+    const diff = gendiff(file1Path, file2Path, 'plain');
+    expect(diff).toBe(result);
+});
+
+test('yml check stylish', () => {
+    const file1Path = getFixturePath('file1.yml');
+    const file2Path = getFixturePath('file2.yml');
+    const resultPath = getFixturePath('result.plain.txt');
+    const result = readFileSync(resultPath, 'utf8');
+    const diff = gendiff(file1Path, file2Path, 'plain');
     expect(diff).toBe(result);
 });
