222e5d3fdf0e3e8ac22e88486a2bd4f79f7bdc39
mag <polina.arutyunyan.08@mail.ru>
2023-06-27 19:17:47 +0300

    Revert "add export default gendiff"
    This reverts commit 49728400efa0c02c2b03e520034eee5ab1574927.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,16 +3,14 @@
 import { program } from 'commander';
 import gendiff from '../src/index.js';
 
-export default () => {
-    program
-        .description('Compares two configuration files and shows a difference.')
-        .option('-V, --version', 'output the version number')
-        .option('-f, --format <type>', 'output format', 'stylish')
-        .argument('<filepath1>')
-        .argument('<filepath2>')
-        .action((filepath1, filepath2, { format }) => {
-            console.log(gendiff(filepath1, filepath2, format));
-        });
+program
+    .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .action((filepath1, filepath2, { format }) => {
+        console.log(gendiff(filepath1, filepath2, format));
+    });
 
-    program.parse();
-};
+program.parse();
