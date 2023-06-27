b5d5b8213720d9af55c37ca00beedf26ad55e65c
mag <polina.arutyunyan.08@mail.ru>
2023-06-27 19:30:16 +0300

    export default gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,16 @@
 import { program } from 'commander';
 import gendiff from '../src/index.js';
 
-program
-    .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format', 'stylish')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .action((filepath1, filepath2, { format }) => {
-        console.log(gendiff(filepath1, filepath2, format));
-    });
+export default () => {
+    program
+        .description('Compares two configuration files and shows a difference.')
+        .option('-V, --version', 'output the version number')
+        .option('-f, --format <type>', 'output format', 'stylish')
+        .argument('<filepath1>')
+        .argument('<filepath2>')
+        .action((filepath1, filepath2, { format }) => {
+            console.log(gendiff(filepath1, filepath2, format));
+        });
 
-program.parse();
+    program.parse();
+};
