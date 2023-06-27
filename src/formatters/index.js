4665ee7c2bfc39db5fb80647b99089d452cbd0e4
mag <polina.arutyunyan.08@mail.ru>
2023-06-27 18:06:42 +0300

    add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,15 @@
 import getStylish from './stylish.js';
 import getPlain from './plain.js';
+import getJSON from './json.js';
 
 const getFormatted = (diff, format) => {
-    if (format === 'stylish') {
+    switch (format) {
+    case 'plain':
+        return getPlain(diff);
+    case 'json':
+        return getJSON(diff);
+    default:
         return getStylish(diff);
     }
-    return getPlain(diff);
 };
 export default getFormatted;
